61b2844f9cef0cd27a8ffe45272ab71597140904
erictalv <eric.talviste@tptlive.ee>
2021-01-28 15:20:15 +0200

    added relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,6 @@ class User extends Authenticatable
      */
     public function articles()
     {
-        return $this->hasMany('App\Article');
+        return $this->hasMany(Article::class);
     }
 }
