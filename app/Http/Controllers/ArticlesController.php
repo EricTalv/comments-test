cc556eb18eed6607ec458923ee9ee191a22026d2
erictalv <eric.talviste@tptlive.ee>
2021-01-28 15:13:23 +0200

    index route

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
+
 
 class ArticlesController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return view('articles.index', ['articles' => Article::all() ]);
     }
 
     /**
